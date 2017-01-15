f2446246df110d390b7f043ed5556dc5e5610288
rmoro <moro1422@mylaurier.ca>
2017-01-15 21:11:45 +0700

    Initial Commit

---
+++ maze_solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maze_solver
+//
+//  Created by user on 2017-01-15.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
